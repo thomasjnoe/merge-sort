8f19fd5856c1d7ac82c1965ac78553b3f2408679
Thomas Noe <thomasjnoe@gmail.com>
2014-08-09 14:47:29 -0400

    Adding comments

---
+++ mergesort.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+def merge_sort(arr)
+	if arr.size > 1
+		left, right = merge_sort(arr[0..arr.size/2-1]), merge_sort(arr[arr.size/2..-1])  # Split array in half
+		return merge(left,right)  # Merge the halved array
+	end
+	arr  # Return original array if its size is 1 or less
+end
+
+def merge(l, r, merged_list = [])
+	while l.size > 0 && r.size > 0  # while both arrays are populated...
+		merged_list << (l[0] < r[0] ? l.shift : r.shift)  # Compare the first two items in each list.  Move the smaller of the two into the merged list and remove it from the original array
+	end
+	merged_list += l.size == 0 ? r : l  # Combine whichever list is not empty with the merged list
+	merged_list
+end
+
+arr = [38,27,43,3,-9,82,10,15,107,39,455,6,22,11]
+
+puts merge_sort(arr)
